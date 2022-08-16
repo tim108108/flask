fff38f36692d1f065fde0ba4c999ba0d971521d0
tim108108 <tim108108@gmail.com>
2022-08-16 18:10:19 +0800

    0816

--- main.py
+++ main.py
@@ -3,14 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-#	return 'Flask Dockerized'
-	return 'hi'
-
-@app.route('/home')
-def home():
-	return render_template('home.html')
+@app.route('/', methods=['GET'])
+def index():
+	return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8888)
+ 
